426c68c24343bbf512b7d2f519cd63de97580662
Charlie Brown <stieizc.33@gmail.com>
2014-02-09 17:50:25 +0800

    add time, improve record

--- record.h
+++ record.h
@@ -1,8 +1,24 @@
 #ifndef RECORD_H
 #define RECORD_H
 
-typedef struct Record {
+#include <asm/types.h>
 
-} Record, *pRecord;
+struct time {
+    __u32 sec;
+    __u32 nanosec;
+};
+
+struct record {
+    __u64 offset;
+    __u32 len;
+    __u32 marks; /* mark if this is merged */
+    struct time Q;
+    struct time G;
+    struct time I;
+    struct time D;
+    struct time M;
+    struct time C;
+    struct record *next;
+};
 
 #endif /* end of include guard: RECORD_H */
